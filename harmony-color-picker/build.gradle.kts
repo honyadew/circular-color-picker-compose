c2d101e980bdca251995b9f7a49f93dc6c806816
HonyaDev <Trolofoks@gmail.com>
2023-06-29 11:37:30 +0700

    Java 11 now in module gradle.

--- harmony-color-picker/build.gradle.kts
+++ harmony-color-picker/build.gradle.kts
@@ -25,11 +25,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
@@ -64,7 +64,7 @@ afterEvaluate{
                 from(components["release"])
                 groupId = "com.github.HonyaDew"
                 artifactId = "circular-color-picker-compose"
-                version = "1.0"
+                version = "1.1.2"
             }
         }
     }
